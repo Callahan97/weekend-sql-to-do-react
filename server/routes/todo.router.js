66dd2a66b0a63b128aeffd091ee897e2c37c00e5
Conor Callahan <conorcallahan97@outlook.com>
2024-05-29 21:37:36 -0500

    added a post route that works with postman data additions but will only permit them if the data is in JSONformat.

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) =>{
     const sqlText =`SELECT * FROM tasks ORDER BY name, location DESC;`;
     pool.query(sqlText)
     .then((result) => {
-        console.log(`Got tasks  info back from database`, result);
+        console.log(`Got tasks info back from database`, result);
         res.send(result.rows);
     })
     .catch((error) => {
@@ -16,7 +16,22 @@ router.get('/', (req, res) =>{
     })
 })
 // POST
-router.post()
+router.post('/', (req, res) => {
+    const task = req.body;
+    console.log(task, 'task');
+    const sqlText = `INSERT INTO tasks (name, location)
+    VALUES ($1, $2)`;
+
+    pool.query(sqlText, [task.name, task.location])
+    .then((result) => {
+        console.log(`Added a task to the database`, task);
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500);
+    })
+});
 // PUT
 
 // DELETE
