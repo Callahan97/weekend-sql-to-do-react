07f8f9eabe9f3a2d6eef25a95b9198c3f391257d
Conor Callahan <conorcallahan97@outlook.com>
2024-06-02 20:01:24 -0500

    Star ing my react code for the clientside code with a GET and the start of a Post.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,58 @@
-import {useState} from 'react';
-
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 function App () {
   
+const fetchTasks = () => {
+  axios({
+    method: 'GET',
+    url: '/api/tasks'
+  }).then((response) => {
+    console.log(response);
+    console.log(response.data);
+    setTaskList(response.data);
+  }).catch((error) => {
+    console.log(error);
+  });
+}
+
+const [taskList, setTaskList] = useState([]);
+
+
+useEffect(fetchTasks, []);
+
+// Will use later
+// const [newTaskName, setNewTaskName] = useState('');
+// const [newTaskLocation, setNewTaskLocation] = useState('');
+
+const addTask = (event) => {
+  event.preventDefault();
+
+  axios({
+    method:
+    url:
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div>
-      <h1>TO DO APP</h1>
+      <h1>TO DO Checklist</h1>
     </div>
   );
 
